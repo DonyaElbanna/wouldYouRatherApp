cb6ae97a00d1ffb965b7a73d14130ac40cc38b48
Donya Elbanna <donyaelbanna@yahoo.com>
2021-08-07 15:50:36 +0200

    authStyle

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { setAuthedUser } from "../actions/authedUser";
-import Container from 'react-bootstrap/Container'
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
+// import Container from 'react-bootstrap/Container'
+// import Card from 'react-bootstrap/Card'
+// import Button from 'react-bootstrap/Button'
+// import Form from 'react-bootstrap/Form'
+import { Container, Card, Button, Form, Row, Col } from 'react-bootstrap'
 
 
 function mapStateToProps({ users }) {
@@ -17,10 +19,12 @@ class Auth extends Component {
     handleSubmit = (e) => {
         e.preventDefault();
     
-        const  authedUser= this.authedUser.value;
+        const authedUser= this.authedUser.value;
     
-        if (authedUser !== "") {
+        if (authedUser) {
           this.props.dispatch(setAuthedUser(authedUser));
+        } else {
+          alert('Please sign in first')
         }
       };
 
@@ -31,28 +35,44 @@ class Auth extends Component {
 
         return (
           <Container fluid className="text-center m-auto">
-            <Card className="m-auto" style={{ width: "100%", height: '100vh'}} >
-              <Card.Title>Welcome to the game</Card.Title>
-              <Card.Text></Card.Text>
-              <Card.Img variant="top" className="m-auto" style={{ width: "30rem"}} src="/wydpic.png" />
+            <Card className="m-auto" style={{ width: "100%", height: "100vh" }}>
+              <Card.Title style={{ marginTop: "40px", marginBottom: "20px" }}>
+                Welcome to the game
+              </Card.Title>
+              <Card.Img
+                variant="top"
+                className="m-auto"
+                style={{ width: "25rem" }}
+                src="/wydpic.png"
+              />
               <Card.Body>
                 <footer className="blockquote-footer">
                   You have to sign in to play the game
                 </footer>
-                <form onSubmit={this.handleSubmit}>
-              <label>Select a User: &nbsp; &nbsp;</label>
-              <select
-                ref={(id) => (this.authedUser = id)}
-                style={{ height: "30px" }}
-              >
-                {users.map((user) => (
-                  <option key={user.id}>{user.name}</option>
-                ))}
-              </select>
-              <Button type='submit' variant="primary">
-                Login
-              </Button>
-            </form>
+                <Form className="m-auto">
+                  <Row
+                    className="align-items-center justify-content-md-center"
+                    style={{ marginTop: "60px" }}
+                  >
+                    <Col md="auto">
+                      <Form.Label>Select a User</Form.Label>
+                    </Col>
+                    <Col md="auto">
+                      <Form.Select ref={(id) => (this.authedUser = id)}>
+                        <option>Choose...</option>
+                        {users.map((user) => (
+                          <option key={user.id}>{user.name}</option>
+                        ))}
+                      </Form.Select>
+                    </Col>
+                    <Col md="auto">
+                      <Button type="submit"
+                              onClick={this.handleSubmit}>
+                        Login
+                      </Button>
+                    </Col>
+                  </Row>
+                </Form>
               </Card.Body>
             </Card>
           </Container>
