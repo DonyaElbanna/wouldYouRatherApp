d2d1dcaf7fb93d51111601b48ccd9452dcd164d9
kareemwezza <kareemfouad27@gmail.com>
2021-08-15 17:28:24 +0200

    routing fixed

--- src/components/Navig.js
+++ src/components/Navig.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { removeAuthedUser } from "../actions/authedUser";
 import { Navbar, Nav, Image, Button } from "react-bootstrap";
-import { withRouter } from "react-router-dom";
+import { NavLink, withRouter } from "react-router-dom";
 
 function mapStateToProps({ authedUser, users }) {
   return {
@@ -22,40 +22,61 @@ class Navig extends Component {
     const { name, avatar } = this.props;
     // console.log("Nav Data: ", this.props);
     // console.log(this.props.history.location.pathname)
-    const location = this.props.history.location.pathname
+    const location = this.props.history.location.pathname;
 
     return (
-      <Navbar id='navig' bg="primary" variant="dark" style={{ marginTop: "5px", height:'65px' }}>
-          <Navbar.Brand href="#home" style={{ fontSize: "30px", }}>
-            <Image
-              src={avatar}
-              style={{ width: "60px", height: "60px", marginLeft: "40px", marginRight: '10px' }}
-              roundedCircle
-            />
-            {`Hi, ${name}!`}
-          </Navbar.Brand>
-          <Nav className="text-center m-auto" style={{ fontSize: "25px" }} 
-          activeKey= {location === '/home' || location === '/'
-                     ? '#home'
-                     : location === '/add'
-                     ? '#add'
-                     : location === '/leaderboard'
-                     ? '#leaderboard'
-                     : '#home' }
+      <Navbar
+        id="navig"
+        bg="primary"
+        variant="dark"
+        style={{ marginTop: "5px", height: "65px" }}
+      >
+        <Navbar.Brand href="#home" style={{ fontSize: "30px" }}>
+          <Image
+            src={avatar}
+            style={{
+              width: "60px",
+              height: "60px",
+              marginLeft: "40px",
+              marginRight: "10px",
+            }}
+            roundedCircle
+          />
+          {`Hi, ${name}!`}
+        </Navbar.Brand>
+        <Nav
+          className="text-center m-auto"
+          style={{ fontSize: "25px" }}
+          activeKey={
+            location === "/home" || location === "/"
+              ? "#home"
+              : location === "/add"
+              ? "#add"
+              : location === "/leaderboard"
+              ? "#leaderboard"
+              : "#home"
+          }
+        >
+          <Nav.Link as={NavLink} to="/home" style={{ marginRight: "50px" }}>
+            Home
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/add" style={{ marginRight: "50px" }}>
+            Add Poll
+          </Nav.Link>
+          <Nav.Link
+            as={NavLink}
+            to="/leaderboard"
+            style={{ marginRight: "50px" }}
           >
-            <Nav.Link href="#home" style={{ marginRight: "50px" }}>
-              Home
-            </Nav.Link>
-            <Nav.Link href="#add" style={{ marginRight: "50px" }}>
-              Add Poll
-            </Nav.Link>
-            <Nav.Link href="#leaderboard" style={{ marginRight: "50px" }}>
-              Leaderboard
-            </Nav.Link>
-          </Nav>
-          <Button onClick={this.logOut} style={{ fontSize: "18px", marginRight: "40px" }}>
-            Logout
-          </Button>
+            Leaderboard
+          </Nav.Link>
+        </Nav>
+        <Button
+          onClick={this.logOut}
+          style={{ fontSize: "18px", marginRight: "40px" }}
+        >
+          Logout
+        </Button>
       </Navbar>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import reducer from './reducers'
-import middleware from './middleware'
-import { HashRouter } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+import middleware from "./middleware";
+import { BrowserRouter, HashRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const store = createStore(reducer, middleware)
+const store = createStore(reducer, middleware);
 
 ReactDOM.render(
-  <HashRouter>
-      <Provider store={store}>
-         <App />
-      </Provider>
-  </HashRouter>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
